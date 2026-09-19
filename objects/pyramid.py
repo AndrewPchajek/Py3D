3c9cd5031a3b19e5887cd780e7e760aa0fba39f9
Andrew Pchajek <apchajek1@gmail.com>
2026-09-18 21:03:23 -0500

    fix: make the pyramid centered by default

--- objects/pyramid.py
+++ objects/pyramid.py
@@ -6,11 +6,11 @@ from vector3 import Vector3
 
 def create_pyramid() -> Object3D:
     vertices = [
-        Vector3(-1, 0, -1),
-        Vector3(-1, 0, 1),
-        Vector3(1, 0, 1),
-        Vector3(1, 0, -1),
-        Vector3(0, 2, 0),
+        Vector3(-1, -1, -1),
+        Vector3(-1, -1, 1),
+        Vector3(1, -1, 1),
+        Vector3(1, -1, -1),
+        Vector3(0, 1, 0),
     ]
     triangles = [(3, 0, 1), (2, 3, 1), (3, 4, 0), (0, 4, 1), (1, 4, 2), (2, 4, 3)]
     return Object3D(Mesh(vertices, triangles), RED)
